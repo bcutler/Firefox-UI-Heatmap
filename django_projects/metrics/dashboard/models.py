d47b5705249f47a4044bc3283e0657c4390538c6
blake <blake@host-7-234.mv.mozilla.com>
2010-08-02 18:07:31 -0700

    colored heatmap according to perc

--- django_projects/metrics/dashboard/models.py
+++ django_projects/metrics/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from math import log
 
 # Create your models here.
 class Heatmap(models.Model):
@@ -11,14 +12,12 @@ class Heatmap(models.Model):
 	clicks_per_user = models.DecimalField(max_digits=5, decimal_places=5)
 	clicks_per_user_median =models.DecimalField(max_digits=5, decimal_places=5)
 	
-	def heat(self):
-    #$normalize = (log($freq+1)/log($max))
-    #$h = 360 * (1 - $normalize)
-    #$s = $normalize * 75
-    #$l = $normalize * 50
-    #return "hsl(" .$h ."," .$s ."%," .$l ."%)"	
-		return true
+	def heat_perc(self):
+		normalize = log((self.perc * 100) + 1 )/ log(100)
+		h = 360 * (1 - normalize)
+		s = normalize * 75
+		l = normalize * 50
+		return "hsl(" + str(h) + "," + str(s) +"%," + str(l) + "%)"	
 		
-	
 	def __unicode__(self):
 		return 'A heatmap of id %d' % (self.id)
--- django_projects/metrics/dashboard/views.py
+++ django_projects/metrics/dashboard/views.py
@@ -28,65 +28,60 @@ def heatmap(request):
 	title = 'Firefox Tab Heatmap'
 	items = Heatmap.objects.all()
 	
-	firefox_button = items[4]
-	tab_scroll_left = items[43]
-	tab_scroll_right = items[44]	
-	new_tab_button = items[45]
-	list_all_tabs = items[46]			
-	
-	back = items[26]
-	forward = items[27]
-	recent_history_drop_down = items[28]
-	reload_button = items[29]
-	stop = items[30]
-	home = items[31]
-	
-	site_id_normal = items[54]
-	site_id_ev = items[55]
-	site_id_ssl = items[56]		
-
-	url_enter_key = items[57]
-	url_go_button = items[58]	
-	url_search_enter_key = items[59]	
-	url_search_go_button = items[59]		
-	most_visited_drop_down = items[61]
-	rss = items[32]
-	bookmark_star = items[33]
-	bookmarks_star_edit = items[34]
-	bookmarks_star_remove = items[35]	
-	
-	search_drop_down = items[48]
-	search_drop_down_select = items[49]	
-	search_enter_key = items[50]
-	search_go_button = items[51]
-	
-	bookmarks_button = items[68]
-	feedback = items[63]
-	
-	scroll_up = items[36]
-	scroll_down = items[37]
-	scroll_vertical_bar = items[38]	
-	
-	scroll_left = items[39]
-	scroll_right = items[40]
-	scroll_horizontal_bar = items[41]
-		
-	tabs_on_top = items[0]
-	menu_bar = items[1]
-	bookmarks_bar = items[2]
-	status_bar = items[3]
-
+	for item in items:
+		item.color = item.heat_perc()
+		item.perc = round((item.perc * 100))
 			
 	t = loader.get_template('heatmap.html')
-	c = Context({
-	    'title': title,
+	c = Context({ 'title': title,
 	    'items':items,
-	    'firefox_button':firefox_button,
 			'firefox_button':items[4],
 			'tab_scroll_left':items[43],
 			'tab_scroll_right':items[44],	
 			'new_tab_button':items[45],
-			'list_all_tabs':items[46],	    
+			'list_all_tabs':items[46],	  
+			
+			'back':items[26],
+			'forward':items[27],
+			'recent_history_drop_down':items[28],
+			'reload_button':items[29],
+			'stop':items[30],
+			'home':items[31],
+			
+			'site_id_normal':items[54],
+			'site_id_ev':items[55],
+			'site_id_ssl':items[56],		
+			
+			'url_enter_key':items[57],
+			'url_go_button':items[58],	
+			'url_search_enter_key':items[59],	
+			'url_search_go_button':items[59],		
+			'most_visited_drop_down':items[61],
+			'rss':items[32],
+			'bookmark_star':items[33],
+			'bookmarks_star_edit':items[34],
+			'bookmarks_star_remove':items[35],	
+			
+			'search_drop_down':items[48],
+			'search_drop_down_select':items[49],	
+			'search_enter_key':items[50],
+			'search_go_button':items[51],
+			
+			'bookmarks_button':items[68],
+			'feedback_button':items[63],
+			
+			'scroll_up':items[36],
+			'scroll_down':items[37],
+			'scroll_vertical_bar':items[38],	
+			
+			'scroll_left':items[39],
+			'scroll_right':items[40],
+			'scroll_horizontal_bar':items[41],
+			
+			'tabs_on_top':items[0],
+			'menu_bar':items[1],
+			'bookmarks_bar':items[2],
+			'status_bar':items[3],
 	})
 	
 	return HttpResponse(t.render(c))
